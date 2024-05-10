be547c3f64cdc31fca7b3c5417ec639c0c21bd47
hongZhou443 <hongzhou2025@u.northwestern.edu>
2024-05-10 14:50:24 -0500

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
 
   const [columnOptions, setColumnOptions] = useState([]);
   const [sensitiveColumn, setSensitiveColumn] = useState("");
+  const [indexColumn, setIndexColumn] = useState("");
 
   const handleSubmit = () => {
     console.log("Submit form!")
@@ -14,6 +15,7 @@ function App() {
 
   const handleUpload = () => {
     console.log("File uploaded!")
+    // encode file
     // updates column Options
   }
 
@@ -21,18 +23,27 @@ function App() {
     console.log("Sensitive column updated!")
   }
 
+
+  const handleIndexColumnSelect = () => {
+    console.log("Index column updated!")
+  }
+
   return (
     <div>
       <form className="tool-form" onSubmit={handleSubmit}>
-      <label>
+        <label>
           Sensitive Column
           <select value={sensitiveColumn} onChange={handleSensitiveColumnSelect}>
-            {
-              columnOptions.map(option => <option value={option}>{option}</option>)
-            }
+            {columnOptions.map(option => <option value={option}>{option}</option>)}
           </select>
         </label>
-        <input type="file" onChange={handleUpload}/>
+        <label>
+          Index Column
+          <select value={indexColumn} onChange={handleIndexColumnSelect}>
+            {columnOptions.filter(option => option != sensitiveColumn).map(option => <option value={option}>{option}</option>)}
+          </select>
+        </label>
+        <input type="file" onChange={handleUpload} />
         <input type="submit" value="Submit" />
       </form>
     </div>
